a30039ca1c62a25aca2d13212ac7f0430af64b2f
Mark Julien Hahn <mail@markjulienhahn.de>
2023-01-20 17:40:30 +0100

    ...

--- components/AboutSliderInner.js
+++ components/AboutSliderInner.js
@@ -2,35 +2,56 @@ import { useState, useEffect, useRef } from "react";
 import { useSwiper, useSwiperSlide } from "swiper/react";
 import Image from "next/image";
 
-const AboutSliderInner = ({ i, length, image, prevSlide, nextSlide, currentIndex, setCurrentIndex }) => {
+const AboutSliderInner = ({
+  i,
+  length,
+  image,
+  prevSlide,
+  nextSlide,
+  currentIndex,
+  setCurrentIndex,
+}) => {
   const swiper = useSwiper();
   const swiperSlide = useSwiperSlide();
 
   useEffect(() => {
-    swiperSlide.isActive
-      ? setCurrentIndex(i)
-      : "";
+    swiperSlide.isActive ? setCurrentIndex(i) : "";
   });
-
 
   return (
     <>
       <div className={"swiperNav"}>
-        <div className={"swiperPrev"} onClick={ currentIndex == 0 ? prevSlide : () => swiper.slidePrev()}></div>
-        <div className={"swiperNext"} onClick={ currentIndex == length -1 ? nextSlide : () => swiper.slideNext()}></div>
+        <div
+          className={"swiperPrev"}
+          onClick={currentIndex == 0 ? prevSlide : () => swiper.slidePrev()}
+        ></div>
+        <div
+          className={"swiperNext"}
+          onClick={
+            currentIndex == length - 1 ? nextSlide : () => swiper.slideNext()
+          }
+        ></div>
       </div>
       <div>
-      {image.dimensions.aspectRation <= 1 ? (
-        <Image src={image.url} loading="eager" />
-      ) : (
-        <Image
-          src={image.url}
-          layout="fill"
-          objectFit="contain"
-          loading="eager"
-          className="portraitImage"
-        />
-      )}
+        {image.dimensions.aspectRation <= 1 ? (
+          <Image
+            src={image.url}
+            layout="responsive"
+            objectFit="contain"
+            height="100"
+            width="100"
+            loading="eager"
+          />
+        ) : (
+          <Image
+            src={image.url}
+            layout="responsive"
+            objectFit="contain"
+            height="100"
+            width="100"
+            loading="eager"
+          />
+        )}
       </div>
     </>
   );
