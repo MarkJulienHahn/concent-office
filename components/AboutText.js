1ec646477e2414d2a7caf5d9a4047cafc8e7be15
Mark Julien Hahn <mail@markjulienhahn.de>
2023-01-20 16:16:01 +0100

    ...

--- components/AboutText.js
+++ components/AboutText.js
@@ -1,15 +1,20 @@
 import { useSwiper } from "swiper/react";
 import { PortableText } from "@portabletext/react";
 
+import { use100vh } from "react-div-100vh";
+
 const AboutText = ({ entry, english }) => {
   const swiper = useSwiper();
+  const height = use100vh();
+
+
   return (
     <>
       <div className={"swiperNav"}>
         <div className={"swiperPrev"} onClick={() => swiper.slidePrev()}></div>
         <div className={"swiperNext"} onClick={() => swiper.slideNext()}></div>
       </div>
-      <div className="swiperSingleAbout">
+      <div className="swiperSingleAbout" style={{height: `calc(${height}px - 160px`}}>
         <PortableText value={english ? entry.textEn : entry.text} />
       </div>
     </>
